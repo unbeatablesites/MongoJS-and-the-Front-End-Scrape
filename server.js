5beddfbf46350a1d99cb74dd22b82f25f4502e75
Fuzoka <unbeatablesites09@gmail.com>
2018-09-17 23:02:36 -0400

    Hw tests

--- server.js
+++ server.js
@@ -32,3 +32,23 @@ mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true 
 // Routes
 
 // A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+  
