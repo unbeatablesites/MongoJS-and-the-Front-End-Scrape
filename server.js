f5fb3cab8f1b3f6d08280a8c9dfda32683611b1c
Fuzoka <unbeatablesites09@gmail.com>
2018-09-17 23:01:51 -0400

    Hw tests

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
